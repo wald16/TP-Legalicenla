49dabf771c0f57af2e7d5e95fca086b67a1756cb
wald <manuwald16@gmail.com>
2024-04-11 22:04:14 -0300

    first push

---
+++ OneDrive/Escritorio/Practica 3 IP/Ej2.hs
@@ -0,0 +1,46 @@
+-- a)
+absoluto :: Integer -> Integer
+absoluto x | x < 0 = -x
+             | otherwise = x
+
+-- b)
+maximoAbsoluto :: Integer -> Integer -> Integer
+maximoAbsoluto x y | absoluto x  > absoluto y = absoluto x
+                   | otherwise              = absoluto y
+
+-- c)
+maximo3 :: Integer -> Integer -> Integer -> Integer
+maximo3 x y z | x > y && x > z = x
+              | y > x && y > z = y
+              | otherwise  = z
+
+-- d)
+algunoEs0 :: Float -> Float -> Bool
+algunoEs0 x y | x == 0 || y == 0 = True
+              | otherwise        = False
+
+algunoEs0_ :: Float -> Float -> Bool
+algunoEs0_ 0 _  = True
+algunoEs0_ _ 0   = True
+algunoEs0_ _ _ = False
+
+
+-- e)   
+ambosSon0 ::  Float -> Float -> Bool
+ambosSon0 x y = x == 0 && y == 0 
+
+ambosSon0_  :: Float -> Float -> Bool
+ambosSon0_ 0 0 = True
+ambosSon0_ _ _ = False
+
+-- f)
+mismoIntervalo  :: Float -> Float -> Bool
+mismoIntervalo x y | (x <= 3 && y <= 3) || (x > 3 && x <= 7 && y > 3 && y <= 7) || (x > 7 && y >= 5) = True
+                   | otherwise = False
+
+-- g)
+sumaDistintos :: Int -> Int -> Int -> Int
+sumaDistintos a b c | a  /= b && a /= c && b /= c = a + b + c
+                    | a == b && a /= c = c
+                    | a /= b && b == c = a
+                    | otherwise = b
